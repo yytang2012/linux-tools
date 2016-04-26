d1278b116554be77d9a9dc291d92f15630a92114
Yutao Tang <yutang@nec-labs.com>
2016-04-26 18:16:27 -0400

    automatically add linux-tool to /home/ami/yutang/linux-tools/common:/home/ami/yutang/linux-tools/nec-AM13-02:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games

--- common/linuxSetup.py
+++ common/linuxSetup.py
@@ -2,12 +2,10 @@
 # coding=utf-8
 import os
 
-vimrcStr = """
-set undofile 
-set undodir=%s
-""";
 
-def linuxSetup():
+def vimrcSetup():
+    vimrcStr = """ set undofile 
+    set undodir=%s """;
     undoDir = '~/.vim/undo'
     undoDir = os.path.expanduser(undoDir);
     if os.path.isdir(undoDir) == False:
@@ -16,6 +14,23 @@ def linuxSetup():
     cmd = 'echo "%s" >> ~/.vimrc' %s;
     os.system(cmd);
 
+def bashrcSetup():
+    bashrcPath = "~/.bashrc";
+    bashrcPath = os.path.expanduser(bashrcPath);
+    linuxToolsDir = "~/linux-tools/";
+    linuxToolsDir = os.path.expanduser(linuxToolsDir);
+    if os.path.isdir(linuxToolsDir) == False:
+        linuxToolsDir = os.getcwd();
+        if os.path.isdir(linuxToolsDir) == False:
+            print("Cannot find the directory");
+            return -1;
+    path1 = os.path.join(linuxToolsDir, 'common');
+    path2 = os.path.join(linuxToolsDir, 'sshconnection');
+    cmd = 'echo "export PATH=%s:%s:$PATH" >> ~/.bashrc'%(path1, path2);
+    os.system(cmd);
+    
+
 if __name__ == '__main__':
-    linuxSetup();
+    vimrcSetup();
+    bashrcSetup();
 
