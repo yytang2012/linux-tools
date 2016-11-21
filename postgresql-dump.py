49b51eb1974d3d068bdd96e634321f40246ef487
yutang <yutang@nec-labs.com>
2016-11-21 16:57:24 -0500

    refactor the code of  postgresql-restore and dump

--- postgresql-dump.py
+++ postgresql-dump.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python
 # coding=utf-8
-import time;
+import time
+
+host_name = 'amsec13-02'
+data_dir = '/Data/psql_dump/data_dump_0/'
+
+
+def db_configuration(host_name='amsec13-02'):
+    if host_name == 'amsec13-02':
+        password = "isa-REL-aero"
+    elif host_name == 'amsec12-05':
+        password = "isaisa"
+    else:
+        password = ''
+    return password
 
 
 def get_database_name_list(startDate='01/15/2016', days=1):
     import datetime
-    database_names = [];
+    database_names = []
     for i in range(0, days):
-        d = datetime.datetime.strptime(startDate, '%m/%d/%Y') + datetime.timedelta(i);
-        # s = "aero_%Y_%02m_%02d"
-        s = 'r1508_%Y_%02m_%02d'
-        name = d.strftime(s);
-        database_names.append(name);
-    return database_names;
+        d = datetime.datetime.strptime(startDate, '%m/%d/%Y') + datetime.timedelta(i)
+        s = "aero_%Y_%02m_%02d"
+        # s = 'r1508_%Y_%02m_%02d'
+        name = d.strftime(s)
+        database_names.append(name)
+    return database_names
 
 
 def dump_database(database_name):
     import os
-    cmd = 'PGPASSWORD="isaisa" pg_dump -C -h amsec12-05 -U  postgres %s > /Data/psql_dump/data_dump_0/%s'
-    dir = os.getcwd();
+    password = db_configuration(host_name)
+    cmd_template = 'PGPASSWORD="{0}" pg_dump -C -h {1} -U  postgres {2} > /Data/psql_dump/data_dump_0/{2}'
+    # cmd = 'PGPASSWORD="isa-REL-aero" pg_dump -C -h amsec13-02 -U  postgres %s > /Data/psql_dump/data_dump_0/%s'
     for db in database_name:
-        path = os.path.join(dir, db);
+        path = os.path.join(data_dir, db)
         if not os.path.isfile(path):
-            print("dumping %s" % db);
-            os.system(cmd % (db, db));
+            print("dumping %s" % db)
+            cmd = cmd_template.format(password, host_name, db)
+            os.system(cmd)
         else:
-            print("%s was downloaded" % db);
+            print("%s was downloaded".format(db))
 
 
 if __name__ == '__main__':
-    database_name_list = get_database_name_list('01/06/2016', 28);
-    print(database_name_list);
-    start = time.time();
-    dump_database(database_name_list);
-    end = time.time();
-    print("Elapsed Time = %.2fs" % (end - start));
+    database_name_list = get_database_name_list('08/07/2016', 1)
+    print(database_name_list)
+    start = time.time()
+    dump_database(database_name_list)
+    end = time.time()
+    print("Elapsed Time = %.2fs" % (end - start))
     print("Task complete!")
--- postgresql-restore.py
+++ postgresql-restore.py
@@ -4,48 +4,58 @@ import time;
 
 from misc import get_start_time, get_elapsed_time
 
+host_name = 'amsec13-02'
 root_dir = '/Data/psql_dump/data_dump_0/'
+password = "isaisa"
 
 
 def get_database_name_list(startDate='01/15/2016', days=1):
     import datetime
-    database_names = [];
+    database_names = []
     for i in range(0, days):
-        d = datetime.datetime.strptime(startDate, '%m/%d/%Y') + datetime.timedelta(i);
-        # s = "aero_%Y_%02m_%02d";
-        s = 'r1508_%Y_%02m_%02d'
-        name = d.strftime(s);
-        database_names.append(name);
-    return database_names;
+        d = datetime.datetime.strptime(startDate, '%m/%d/%Y') + datetime.timedelta(i)
+        if host_name == 'amsec13-02':
+            s = "aero_%Y_%02m_%02d"
+        elif host_name == 'amsec12-05':
+            s = 'r1508_%Y_%02m_%02d'
+        else:
+            s = ''
+
+        name = d.strftime(s)
+        database_names.append(name)
+    return database_names
 
 
 def restore_database(database_list):
     import os
     for database in database_list:
         print(database)
-        start = time.time();
+        start = time.time()
         file_path = os.path.join(root_dir, database)
         if not os.path.isfile(file_path):
             continue
         """Create a database"""
-        cmd = 'PGPASSWORD="isaisa" createdb -h localhost -U postgres %s' % database
+        # cmd = 'PGPASSWORD="isaisa" createdb -h localhost -U postgres %s' % database
+        cmd = 'PGPASSWORD="{0}" createdb -h localhost -U postgres {1}'.format(password, database)
         state = os.system(cmd)
         """Database already exists"""
         if state != 0:
             continue
         """Restore the database"""
-        cmd = 'PGPASSWORD="isaisa" psql -h localhost -U postgres %s < %s' % (database, file_path)
+        # cmd = 'PGPASSWORD="isaisa" psql -h localhost -U postgres %s < %s' % (database, file_path)
+        cmd = 'PGPASSWORD="{0}" psql -h localhost -U postgres {1} < {2}'.format(password, database, file_path)
         os.system(cmd)
-        end = time.time();
+        end = time.time()
         print("%s is restored. Elapsed Time = %.2fs" % (database, end - start))
 
+
 if __name__ == '__main__':
-    start_date = '01/06/2016'
-    days = 28
+    start_date = '08/01/2016'
+    days = 7
     database_name_list = get_database_name_list(start_date, days)
     print(database_name_list)
     start_time = get_start_time()
     restore_database(database_name_list)
-    elapsed_time = get_elapsed_time(start_time);
+    elapsed_time = get_elapsed_time(start_time)
     print("Elapsed Time = %s" % elapsed_time)
     print("Task complete!")
