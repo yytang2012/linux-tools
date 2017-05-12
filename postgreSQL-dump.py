b4aed6f17abf2df89f39d93fc202b2678573baec
yytang2012 <kissingers800@gmail.com>
2017-05-11 21:55:29 -0400

    Added asi-stable database to postgresSQL-dump.py

--- postgreSQL-dump.py
+++ postgreSQL-dump.py
@@ -2,20 +2,17 @@
 # coding=utf-8
 import time
 
-host_name = 'amsec13-03'
+from misc import stopwatch
+
 data_dir = '/Data/psql_dump/data_dump_0/'
 
+host_to_password_dict = {
+    'amsec13-02': ('postgres', "isa-REL-aero"),
+    'amsec13-03': ('postgres', "isaisa"),
+    'amsec12-05': ('postgres', "isaisa"),
+    'asi-stable': ('asi', "isa-REL-crimson"),
 
-def db_configuration(host_name='amsec13-02'):
-    if host_name == 'amsec13-02':
-        password = "isa-REL-aero"
-    elif host_name == 'amsec13-03':
-        password = "isaisa"
-    elif host_name == 'amsec12-05':
-        password = "isaisa"
-    else:
-        password = ''
-    return password
+}
 
 
 def get_database_name_list(startDate='01/15/2016', days=1):
@@ -23,7 +20,8 @@ def get_database_name_list(startDate='01/15/2016', days=1):
     database_names = []
     for i in range(0, days):
         d = datetime.datetime.strptime(startDate, '%m/%d/%Y') + datetime.timedelta(i)
-        s = "beige_%Y_%02m_%02d"
+        s = "crimson_%Y_%02m_%02d"
+        # s = "beige_%Y_%02m_%02d"
         # s = "aero_%Y_%02m_%02d"
         # s = 'r1508_%Y_%02m_%02d'
         name = d.strftime(s)
@@ -31,26 +29,26 @@ def get_database_name_list(startDate='01/15/2016', days=1):
     return database_names
 
 
-def dump_database(database_name):
+def dump_database(host_name, database_name):
     import os
-    password = db_configuration(host_name)
-    cmd_template = 'PGPASSWORD="{0}" pg_dump -C -h {1} -U  postgres {2} > /Data/psql_dump/data_dump_0/{2}'
+    user, password = host_to_password_dict[host_name]
+    cmd_template = 'PGPASSWORD="{password}" pg_dump -C -h {host_name} -U  postgres {db_name} > ' \
+                   '/Data/psql_dump/data_dump_0/{db_name}'
     # cmd = 'PGPASSWORD="isa-REL-aero" pg_dump -C -h amsec13-02 -U  postgres %s > /Data/psql_dump/data_dump_0/%s'
     for db in database_name:
         path = os.path.join(data_dir, db)
         if not os.path.isfile(path):
-            print("dumping {0}".format(db))
-            cmd = cmd_template.format(password, host_name, db)
+            print("dumping {db_name}".format(db_name=db))
+            cmd = cmd_template.format(password=password, host_name=host_name, db_name=db)
             os.system(cmd)
         else:
             print("{0} was downloaded".format(db))
 
 
 if __name__ == '__main__':
-    database_name_list = get_database_name_list('02/01/2017', 28)
+    host_name = 'asi-stable'
+    database_name_list = get_database_name_list('05/01/2017', 3)
     print(database_name_list)
-    start = time.time()
-    dump_database(database_name_list)
-    end = time.time()
-    print("Elapsed Time = %.2fs" % (end - start))
+    with stopwatch('Database dump'):
+        dump_database(host_name, database_name_list)
     print("Task complete!")
